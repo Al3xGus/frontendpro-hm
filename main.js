998ad0dcebdf578cde376968cc38d50f3912df25
Al3xGus <sgura.guslyakov@gmail.com>
2024-01-19 20:31:51 +0300

    Average

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let name = prompt(  "Hello, what's your name?");
 
-alert("Hello, " + name + "! How are you?");
+var number1 = parseFloat(prompt("Введите первое число:"));
+var number2 = parseFloat(prompt("Введите второе число:"));
+var number3 = parseFloat(prompt("Введите третье число:"));
+
+var average = (number1 + number2 + number3) / 3;
+
+alert("Среднее арифметическое: " + average);
