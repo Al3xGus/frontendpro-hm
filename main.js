567533350134cb6c7a31d47b5660d72b9b7c07ae
Al3xGus <sgura.guslyakov@gmail.com>
2024-01-23 20:47:50 +0300

    Calculator

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
+var operation = prompt("Введите операцию: (add, sub, mult, div):");
 
 var num1 = parseFloat(prompt("Введите первое число:"));
 
 var num2 = parseFloat(prompt("Введите второе число:"));
 
-alert(
-    num1 + " + " + num2 + " = " + (num1 + num2) + "\n" +
-    num1 + " - " + num2 + " = " + (num1 - num2) + "\n" +
-    num1 + " * " + num2 + " = " + (num1 * num2) + "\n" +
-    num1 + " / " + num2 + " = " + (num1 / num2)
-);
+switch (operation) {
+    case "add":
+        var result = num1 + num2;
+        alert(num1 + " + " + num2 + " = " + result);
+        break;
+    case "sub":
+        var result = num1 - num2;
+        alert(num1 + " - " + num2 + " = " + result);
+        break;
+    case "mult":
+        var result = num1 * num2;
+        alert(num1 + " * " + num2 + " = " + result);
+        break;
+    case "div":
+        if (num2 !== 0) {
+            var result = num1 / num2;
+            alert(num1 + " / " + num2 + " = " + result);
+        } else {
+            alert("Деление на ноль невозможно.");
+        }
+        break;
+    default:
+        alert("Пожалуйста, введите корректную операцию.");
+}
