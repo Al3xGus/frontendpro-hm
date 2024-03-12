c0c157aa771c3105e4f9dc7b86ef325b1d56911e
Al3xGus <sgura.guslyakov@gmail.com>
2024-03-12 19:42:04 +0200

    ladder

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var length = prompt("Введите длину массива:");
-var array = [];
-for (var i = 0; i < length; i++) {
-    var element = prompt("Введите элемент массива на позицию " + (i + 1) + ":");
-    array.push(element);
-}
-document.write("Начальный массив: " + array.join(", ") + "<br>");
-array.sort();
-document.write("Отсортированный массив: " + array.join(", ") + "<br>");
-array.splice(1, 3);
-document.write("Обновленный массив после удаления элементов со 2 по 4: " + array.join(", "));
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    }
+};
+
+ladder.up().up().down().showStep(); // 1
