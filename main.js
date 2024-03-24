3cc87f666085443db2c327cacdd2ba058d1c084f
Al3xGus <sgura.guslyakov@gmail.com>
2024-03-24 13:30:31 +0200

    generateList

--- main.js
+++ main.js
@@ -1,59 +1,36 @@
-// Часть 1
-function showInfo() {
-    var birthYear = prompt("Введіть ваш рік народження:");
-    if (birthYear === null) {
-        alert("Шкода, що Ви не захотіли ввести свій рік народження.");
-        return;
+function generateList(array) {
+    let result = '';
+
+    function generateInnerList(innerArray) {
+        let innerResult = '';
+        innerResult += '<ul>';
+        innerArray.forEach(item => {
+            if (Array.isArray(item)) {
+                innerResult += generateInnerList(item);
+            } else {
+                innerResult += `<li>${item}</li>`;
+            }
+        });
+        innerResult += '</ul>';
+        return innerResult;
     }
 
-    var city = prompt("Введіть ваше місто проживання:");
-    if (city === null) {
-        alert("Шкода, що Ви не захотіли ввести своє місто.");
-        return;
-    }
+    array.forEach(item => {
+        if (Array.isArray(item)) {
+            result += generateInnerList(item);
+        } else {
+            result += `<ul><li>${item}</li></ul>`;
+        }
+    });
 
-    var favoriteSport = prompt("Введіть ваш улюблений вид спорту:");
-    if (favoriteSport === null) {
-        alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
-        return;
-    }
-
-    var currentYear = new Date().getFullYear();
-    var age = currentYear - birthYear;
-
-    var message = "Ваш вік: " + age + "\n";
-
-    if (city.toLowerCase() === "київ" || city.toLowerCase() === "вашингтон" || city.toLowerCase() === "лондон") {
-        message += "Ти живеш у столиці " + getCountry(city) + "...\n";
-    } else {
-        message += "Ти живеш у місті " + city + "...\n";
-    }
-
-    // Часть 2
-    var sportsAndChampions = {
-        "футбол": "Ліонель Мессі",
-        "баскетбол": "Леброн Джеймс",
-        "теніс": "Рафаель Надаль"
-    };
-
-    if (sportsAndChampions.hasOwnProperty(favoriteSport.toLowerCase())) {
-        message += "Круто! Хочеш стати " + sportsAndChampions[favoriteSport.toLowerCase()] + "?";
-    }
-
-    alert(message);
+    return result;
 }
 
-function getCountry(city) {
-    switch (city.toLowerCase()) {
-        case "київ":
-            return "України";
-        case "вашингтон":
-            return "США";
-        case "лондон":
-            return "Великої Британії";
-        default:
-            return "";
-    }
-}
+const array = [
+    [1, 2, 3],
+    [1, 2, [1.1, 1.2, 1.3]],
+    3
+];
 
-showInfo();
+const htmlList = generateList(array);
+console.log(htmlList);
